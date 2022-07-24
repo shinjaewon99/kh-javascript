956e58c9bd95b8bf0e94acca6507247f441a6ce2
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-07-24 14:44:46 +0900

    Add files via upload

---
+++ ch07_4.js
@@ -0,0 +1,10 @@
+function calc(){
+
+    var y =1;
+   
+    if (y == 0){
+   document.getElementById("res").className = "error"
+   
+}
+    // document.getElementById("res").style="background-color: red; color : white;"
+}
